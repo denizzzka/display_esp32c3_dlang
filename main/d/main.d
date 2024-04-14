2b5417641a66a51be5db199042565dd230195d1d
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-04-14 17:21:09 +0300

    Compilation ok, but without generated bindings. LED blinks

--- main/d/main.d
+++ main/d/main.d
@@ -3,12 +3,38 @@
 //~ #include <stdio.h>
 //~ #include "freertos/FreeRTOS.h"
 //~ #include "freertos/task.h"
+
+// freertos/portmacro.h
+// #define portTICK_PERIOD_MS              ( ( TickType_t ) 1000 / configTICK_RATE_HZ )
+enum CONFIG_FREERTOS_HZ = 1000;
+enum portTICK_PERIOD_MS = 1000 / CONFIG_FREERTOS_HZ;
+
 //~ #include "driver/gpio.h"
-import gpio;
+
+alias esp_err_t = int;
+alias gpio_num_t = int;
+alias uint32_t = int;
+alias TickType_t = int;
+
+enum gpio_mode_t {
+    GPIO_MODE_DISABLE = (0),
+    GPIO_MODE_INPUT = (0x00000001),
+    GPIO_MODE_OUTPUT = (0x00000002),
+    GPIO_MODE_OUTPUT_OD = (((0x00000002)) | ((0x00000004))),
+    GPIO_MODE_INPUT_OUTPUT_OD = (((0x00000001)) | ((0x00000002)) | ((0x00000004))),
+    GPIO_MODE_INPUT_OUTPUT = (((0x00000001)) | ((0x00000002))),
+}
+
+extern(C) esp_err_t gpio_set_level(gpio_num_t gpio_num, uint32_t level);
+extern(C) esp_err_t gpio_reset_pin(gpio_num_t gpio_num);
+extern(C) esp_err_t gpio_set_direction(gpio_num_t gpio_num, gpio_mode_t mode);
+extern(C) void vTaskDelay(const TickType_t xTicksToDelay);
 
 //~ #include "esp_log.h"
 //~ #include "led_strip.h"
 //~ #include "sdkconfig.h"
+
+enum CONFIG_BLINK_PERIOD = 20;
 
 //~ static const char *TAG = "example";
 
@@ -31,7 +57,7 @@ void configure_led()
     //~ ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
     gpio_reset_pin(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_set_direction(BLINK_GPIO, gpio_mode_t.GPIO_MODE_OUTPUT);
 }
 
 extern(C) void app_main()
