c408630dd31520a5ae9e76b0519cc0d039b2ba27
Denis Feklushkin <feklushkin.denis@gmail.com>
2024-04-22 22:40:33 +0300

    cursor fixed

--- main/d/effects.d
+++ main/d/effects.d
@@ -2,21 +2,21 @@ module effects;
 
 immutable wstring[] text_for_typing = [
 //   0123456789012345
-    "-Алло,это ВПЧ-2?",
-    "-Да.",
-    "-Что у вас там", "горит?",
+    "-Алло,это ВПЧ-2?", null,
+    "-Да.", null,
+    "-Что у вас там", "горит?", null,
     "-Взрыв на главн-",
     "ом корпусе! 3-й,",
-    "4-й, между 3 и", "4 блоком.",
-    "А там люди есть?",
+    "4-й, между 3 и", "4 блоком.", null,
+    "А там люди есть?", null,
     "-Да! Подымай ",
-    "начсостав!",
+    "начсостав!", null,
     "-Подымаю. На-",
-    "чальника поднял!",
+    "чальника поднял!", null,
     "-Так всех,всех,",
     "весь офицерский",
     "состав,офицер-",
-    "ский корпус", "подымай!",
+    "ский корпус", "подымай!", null,
 ];
 
 size_t waitBetweenLines;
@@ -32,9 +32,14 @@ void step()
 
     if(curr_letter >= text_for_typing[curr_line].length)
     {
-        waitBetweenLines = 20;
         curr_letter = 0;
         curr_line++;
+
+        if(text_for_typing[curr_line] is null)
+        {
+            curr_line++;
+            waitBetweenLines = 30;
+        }
     }
 }
 
@@ -43,5 +48,5 @@ wstring getCurrState(out size_t lastLetter)
     lastLetter = curr_letter < 16 ? curr_letter : 15;
 
     auto s = text_for_typing[curr_line];
-    return s[0 .. lastLetter];
+    return s[0 .. lastLetter+1];
 }
